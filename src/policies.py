f5d9e55acfebb7617544dce2d26ba72f4379e5c4
Timothy <timothyholz123@gmail.com>
2026-03-26 21:39:31 -0400

    Update policies.py

--- src/policies.py
+++ src/policies.py
@@ -9,3 +9,22 @@ def pickEDD(readyOrders, vehicleCapacity):
 
     possible.sort(key=lambda x: (x.due, x.arrival))
     return possible[0]
+
+
+def pickFCFS(readyOrders, vehicleCapacity):
+    possible = []
+    for order in readyOrders:
+        if order.ready and (not order.dispatched) and order.size <= vehicleCapacity:
+            possible.append(order)
+
+    if not possible:
+        return None
+
+    possible.sort(key=lambda x: x.arrival)
+    return possible[0]
+
+
+POLICIES = {
+    "edd": pickEDD,
+    "fcfs": pickFCFS,
+}
